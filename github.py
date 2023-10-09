23abb13d669647b01d655870f9c1cfdb1eade143
Orban Szilard <orbanszlrd@gmail.com>
2023-10-09 11:51:31 +0200

    Add argparse and refactor the script

--- github.py
+++ github.py
@@ -1,25 +1,81 @@
 #!/usr/bin/env python3
 
 from git import Repo, GitCommandError
+import argparse
 import requests
 import sys
 
-def get_username():
-    return "orbanszlrd" if len(sys.argv) > 1 else input("Please provide GitHub username: ")
 
-def get_token():
-    return sys.argv[1] if len(sys.argv) > 1 else input("Please provide GitHub token: ")
+def main(): 
+    args = get_args()
+
+    username = get_username(args.username)
+    token = get_token(args.token)
+
+    api_url = "https://api.github.com/"
+    query_params = "?sort=name&per_page=100"
+    user_url = f"{api_url}users/{username}"
+    headers = {"Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28"}
+
+    if token:
+        headers["Authorization"] = 'Bearer ' + token
+
+    user = fetch_data(user_url, headers)
+
+    if "message" in user:
+        sys.exit(user["message"])
+
+    repos_url = f"{api_url}user/repos{query_params}&affiliation=owner" if username == "orbanszlrd" and token else f"{api_url}users/{username}/repos{query_params}"
+
+    projects = fetch_data(repos_url, headers)
+    process_projects(username, projects, args.action)
+
+    if "organizations_url" in user:
+        organizations = fetch_data(user["organizations_url"], headers)
+
+        for organization in organizations:
+            if "repos_url" in organization:
+                owner = organization["login"]
+                repos_url = organization["repos_url"] + query_params
+                projects = fetch_data(repos_url, headers)
+                process_projects(owner, projects, args.action)
+
+    sys.exit("Success.")
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Clone and update GitHub repositories")
+    parser.add_argument("-u", "--username")
+    parser.add_argument("-t", "--token")
+    parser.add_argument("-a", "--action", default="list", help='Choices : list, clone, all (default: list)')
+    parser.add_argument("-m", "--menu")
+
+#    parser.print_help()
+
+    return parser.parse_args()
+
+
+def get_username(username):
+    return username or input("Please provide GitHub username: ")
+
+
+def get_token(token):
+    return token or input("Please provide GitHub token (press enter for public repositories): ")
+
 
 def fetch_data(url, headers): 
     response = requests.get(url, headers=headers)
     return response.json()
 
 
-def process_projects(owner, projects):
+def process_projects(owner, projects, action):
     print(f"{owner}: {len(projects)} projects")
     if len(projects) > 0:
-        print_projects(projects)
-        clone_and_update_projects(projects)
+        if action in ("list", "all"):
+            print_projects(projects)
+
+        if action in ("clone", "all"):
+            clone_and_update_projects(projects)
 
 
 def print_projects(projects):
@@ -50,29 +106,4 @@ def clone_and_update_projects(projects):
 
 
 if __name__ == '__main__':
-    username = get_username()
-    api_url = "https://api.github.com/"
-    query_params = "?sort=name&per_page=100"
-    user_url = f"{api_url}users/{username}"
-    headers = {'Accept': 'application/vnd.github+json', 'Authorization': 'Bearer ' + get_token(), "X-GitHub-Api-Version": "2022-11-28"}
-    user = fetch_data(user_url, headers)
-
-    if "message" in user:
-        sys.exit(user["message"])
-
-    repos_url = f"{api_url}user/repos{query_params}&affiliation=owner"
-    projects = fetch_data(repos_url, headers)
-
-    process_projects(username, projects)
-
-    if "organizations_url" in user:
-        organizations = fetch_data(user["organizations_url"], headers)
-
-        for organization in organizations:
-            if "repos_url" in organization:
-                owner = organization["login"]
-                repos_url = organization["repos_url"] + query_params
-                projects = fetch_data(repos_url, headers)
-                process_projects(owner, projects)
-
-    sys.exit("Success.")
+    main()
