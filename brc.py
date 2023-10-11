b14e0db6d2fb9552ccdb5d9858166c04428e93b2
Orban Szilard <orbanszlrd@gmail.com>
2023-10-11 11:09:15 +0200

    Add target directory attribute

--- brc.py
+++ brc.py
@@ -2,6 +2,7 @@
 
 from git import Repo, GitCommandError
 import argparse
+import os
 import requests
 import sys
 
@@ -30,9 +31,9 @@ def main():
 
     if len(projects) > 0 and username != projects[0]["owner"]["login"]:
         owner = projects[0]["owner"]["login"]
-        sys.exit( f"{owner} has on access to {username}'s private repositories")
+        sys.exit( f"{owner} has no access to {username}'s private repositories")
 
-    process_projects(username, projects, args.action)
+    process_projects(username, projects, args.action, args.dir)
 
     if "organizations_url" in user:
         organizations = fetch_data(user["organizations_url"], headers)
@@ -42,17 +43,19 @@ def main():
                 owner = organization["login"]
                 repos_url = organization["repos_url"] + query_params
                 projects = fetch_data(repos_url, headers)
-                process_projects(owner, projects, args.action)
+                process_projects(owner, projects, args.action, args.dir)
 
-    sys.exit("Success")
+    sys.exit("Finished cloning and updating repositories")
 
 
 def get_args():
+    default_dir = os.path.join("projects", "github")
+
     parser = argparse.ArgumentParser(description="Clone and update GitHub repositories")
     parser.add_argument("-u", "--username")
     parser.add_argument("-t", "--token")
     parser.add_argument("-a", "--action", default="list", help='Choices : list, clone, all (default: list)')
-    parser.add_argument("-m", "--menu")
+    parser.add_argument("-d", "--dir", default=default_dir, help=f"Target directory (default: {default_dir})")
     
     return parser.parse_args()
 
@@ -70,14 +73,15 @@ def fetch_data(url, headers):
     return response.json()
 
 
-def process_projects(owner, projects, action):
-    print(f"{owner}: {len(projects)} projects")
+def process_projects(owner, projects, action, target_dir):
+    print(f"username: {owner}")
+    print(f"{len(projects)} repositories")
     if len(projects) > 0:
         if action in ("list", "all"):
             print_projects(projects)
         
         if action in ("clone", "all"):
-            clone_and_update_projects(projects)
+            clone_and_update_projects(projects, target_dir)
 
 
 def print_projects(projects):
@@ -87,21 +91,21 @@ def print_projects(projects):
     print()
 
 
-def clone_and_update_projects(projects):
-    local_path = "projects/github/"
+def clone_and_update_projects(projects, target_dir):
     env = {"GIT_SSH_COMMAND": "ssh -o StrictHostKeyChecking=no"}
 
-    print("\nClone or update projects\n")
     for project in projects :
         print("Clone", project["full_name"])
 
+        target_repo = os.path.abspath(os.path.join(target_dir, project['full_name']))
+
         try:
-            Repo.clone_from(project["ssh_url"], local_path + project['full_name'], env=env)
-            print("Project cloned")
+            Repo.clone_from(project["ssh_url"], target_repo, env=env)
+            print("Cloned to", target_repo)
         except GitCommandError:
-            print("Project already exists")
-            Repo(local_path + project['full_name']).remotes.origin.pull(env=env)
-            print("Project updated")
+            print(f"Project {target_repo} already exists")
+            Repo(target_repo).remotes.origin.pull(env=env)
+            print("Updated")
         except:
             print("Unknown error")
         print()
